c91c03cf80c1681985500a53402f6e4d2bf7444b
adjaballah <asmaedjaballah1@gmail.com>
2024-12-20 01:31:35 +0100

    Add operations to calculate and provide feedback on a user’s monthly savings and potential future savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-monthly_income = int(input("Enter your monthly income :"))
-total_monthly_expenses = int(input("Enter your total monthly expenses :"))
+monthly_income = int(input("Enter your monthly income :$"))
+total_monthly_expenses = int(input("Enter your total monthly expenses :$"))
 monthly_savings = monthly_income - total_monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings *12*0.05)
 
-print("Your monthly savings is : $",monthly_savings)
-print("Projected savings after including interest is : $",projected_savings)    
+print(f"Your monthly savings are: ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")   
