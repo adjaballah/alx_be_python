f673672598e106a52e4d5f35b473822adea20655
adjaballah <asmaedjaballah1@gmail.com>
2025-01-12 17:27:30 +0100

    verification

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,12 +2,12 @@ def perform_operation(num1, num2, operation):
 
   if operation == 'add':
     return num1 + num2
-  elif operation == 'subtract' :
+  elif operation == 'subtract':
     return num1 - num2
-  elif operation == 'multiply' :
+  elif operation == 'multiply':
     return num1 * num2
-  elif operation == 'divide' :
-    if num2==0 :
+  elif operation == 'divide':
+    if num2 == 0:
       return "Error : Division by zero is not allowed."
     return num1 / num2
   else:
